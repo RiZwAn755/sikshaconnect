c9a58abb700339aa859628b5a0eff3b0658a978c
Avaneesh <avaneesh.m.maurya@gmail.com>
2025-11-28 00:16:12 +0530

    created signup api

---
+++ backend/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  // friend list
+  friendlist: [
+  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+],
+//  Incoming friend requests
+  friendRequests: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  ],
+
+  //  Requests user has sent to others
+  sentRequests: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  ]
+
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
