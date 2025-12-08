99041d1f3ce56f0e24ae3c8a4d49823aa4ed6b03
Rizwan <mdrizwan6386@gmail.com>
2025-12-08 21:15:52 +0530

    minor changes

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,19 @@ import cluster from 'cluster';
 import os from 'os';
 import app from './server.js';
 
-const numcpus = os.cpus().length;
-console.log(numcpus);
+// const numcpus = os.cpus().length;
+// console.log(numcpus);
 
-if(cluster.isPrimary){ // primary node serves as load balancer
+// if(cluster.isPrimary){ // primary node serves as load balancer
 
-    for(let i = 0;  i < numcpus; i++){
-        cluster.fork();
-    }
-}else{
+//     for(let i = 0;  i < numcpus; i++){
+//         cluster.fork();
+//     }
+// }else{
+// app.listen(3000, () => {
+//     console.log(`Server running on port 3000 with process id ${process.pid}`);
+// });
+// }
 app.listen(3000, () => {
     console.log(`Server running on port 3000 with process id ${process.pid}`);
 });
-}
