c6525d61d697fad07e908b389ca782aeb210ca26
Avaneesh <avaneesh.m.maurya@gmail.com>
2025-12-08 23:14:27 +0530

    initialised register

---
+++ frontend/src/components/auth/register.jsx
@@ -0,0 +1,44 @@
+import {useState} from "react";
+import axios from "axios";
+
+const Register =()=>{
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] =useState("")
+    const [password, setPassword] = useState("");
+    const baseUrl = import.meta.env.VITE_BASE_URL;
+   
+     const  handleSubmit= async()=>{
+        e.preventDefault();
+         const formData= {name, username, email, password};
+        if(!name || !username || !email || !password){
+            alert("all the fields are required");
+        }
+        const res = await axios.post(`${baseUrl}/api/auth/signup`,{
+            formData
+        })
+        
+        if(!res){
+            alert("unlable to resister");
+        }
+        else{
+            alert("resister successfully")
+        }
+
+     }
+
+    return(
+         <>
+         <h3>Register</h3>
+         <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="enter name" value={name} onChange={e=>setName(e.target.value)} />
+            <input type="text" placeholder="enter username" value={username} onChange={e=>setUsername(e.target.value)}/>
+            <input type="email" placeholder="enter email" value={email} onChange={e=>setEmail(e.target.value)}/>
+            <input type="password" placeholder="enter password" value={password} onChange={e=>setPassword(e.target.value)} />
+            <button type='submit'>Submit</button>
+         </form>
+         </>
+    )
+}
+
+export default Register;
