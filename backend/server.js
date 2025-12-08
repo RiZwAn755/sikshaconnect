8f8319b5d31c9a4e8cc7bc89c390f385b2e511aa
Rizwan <mdrizwan6386@gmail.com>
2025-12-08 22:00:19 +0530

    initialized login fetch

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authRoutes from './routes/auth.route.js';
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 // Routes
 app.use("/api/auth", authRoutes);
 
