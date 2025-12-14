c5617332a98e1043c19aac129504b33451a548ed
Rizwan <mdrizwan6386@gmail.com>
2025-12-14 22:54:50 +0530

    implemented search friend functionality

--- backend/controllers/me.contorller.js
+++ backend/controllers/me.contorller.js
@@ -1,8 +1,8 @@
 import User from "../models/user.model.js";
 
 export const getMe = async (req, resp) => {
-    const username = req.body;
-    const data = await User.findOne(username);
+    const {username} = req.body;
+    const data = await User.findOne({username});
 
     if(data){
         return resp.status(200).json(data);
@@ -10,4 +10,14 @@ export const getMe = async (req, resp) => {
         return resp.status(400).send("No user found");
     }
 
+}
+
+export const search = async(req, resp) =>{
+    const {username} = req.body;
+    const res = await User.findOne({username});
+    if(res){
+       return resp.status(200).json(res);
+    }else{
+        return resp.status(404).json({"message":"no user with this username"});
+    }
 }
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { getMe } from '../controllers/me.contorller.js';
+import { getMe, search} from '../controllers/me.contorller.js';
 
 const router = express.Router();
 router.get("/me", getMe);
+router.post("/search", search);
 
 export default router;
