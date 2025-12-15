8debb9a001d99104c72ee7e17d513f883e324a08
Rizwan <mdrizwan6386@gmail.com>
2025-12-16 03:41:52 +0530

    initialized search , add , accept friend request

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import authRoutes from './routes/auth.route.js';
 import paymentRoutes from './routes/payment.route.js'
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.route.js";
+import friendshipRoutes from "./routes/friendship.route.js"
 import cors from "cors";
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/user",  userRoutes);
+app.use("/api/friendship", friendshipRoutes)
 
 
 
