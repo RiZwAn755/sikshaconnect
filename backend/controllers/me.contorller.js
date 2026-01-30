9d44a3567e17c5b605deec9e72da626242657696
Avaneesh <avaneesh.m.maurya@gmail.com>
2026-01-31 00:23:33 +0530

    added debounce and regex technique

--- backend/controllers/me.contorller.js
+++ backend/controllers/me.contorller.js
@@ -25,14 +25,37 @@ export const getMe = async (req, res) => {
     }
 };
 
-export const search = async (req, resp) => {
+// export const search = async (req, resp) => {
+//     const { username } = req.body;
+//     const res = await User.findOne({ username });
+//     if (res) {
+//         return resp.status(200).json(res);
+//     } else {
+//         return resp.status(404).json({ "message": "no user with this username" });
+//     }
+// }
+export const search = async (req, res) => {
+  try {
     const { username } = req.body;
-    const res = await User.findOne({ username });
-    if (res) {
-        return resp.status(200).json(res);
-    } else {
-        return resp.status(404).json({ "message": "no user with this username" });
+
+    if (!username || username.length < 1) {
+      return res.status(400).json({ users: [] });
     }
-}
+
+    const users = await User.find({
+      username: {
+        $regex: username,     
+        $options: "i",       
+      },
+    })
+      .select("username _id") // send only required fields
+      .limit(4);              
+
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 
    
